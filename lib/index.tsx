e0f23261c850f08f54926bc77332f449b7ec5c41
fribble186 <psj124@qq.com>
2024-06-07 09:27:38 +0800

    chore: optimization

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,101 +1,149 @@
-import React from 'react';
-import ChatInputBox from './components/chatInputBox';
-import MessageBox, { MessageBoxContainer, Avatar, Message } from './components/messageBox';
-import styled from 'styled-components';
-import { setClassName, getComponent, getSlot } from './utils';
-import { nanoid } from 'nanoid';
-import Loading from './components/loading';
+import React, { useLayoutEffect, useRef, useState } from "react";
+import ChatInputBox from "./components/chatInputBox";
+import MessageBox, {
+  MessageBoxContainer,
+  Avatar,
+  Message,
+} from "./components/messageBox";
+import styled from "styled-components";
+import { setClassName, getComponent, getSlot } from "./utils";
+import Loading from "./components/loading";
 import UserIcon from "./icons/user";
 import OpenaiIcon from "./icons/openai";
-import type { ChatgptComponentProps } from './type';
+import { throttle } from "lodash";
+import type { ChatgptComponentProps } from "./type";
 
 const DefaultMeAvatar = <UserIcon />;
 const DefaultGptAvatar = <OpenaiIcon />;
 const IndexContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  font-family: Arial, sans-serif;
+  textarea {
     font-family: Arial, sans-serif;
-    textarea {
-        font-family: Arial, sans-serif;
-    }
+  }
 `;
 const MessagesScrollContainer = styled.div`
-    flex: 1;
-    overflow-y: auto;
-    &::-webkit-scrollbar {
-        width: 3px;
-        height: 3px;
-    }
-    &::-webkit-scrollbar-thumb {
-        background-color: #dddddd;
-        border-radius: 6px;
-    }
+  flex: 1;
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 3px;
+    height: 3px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #dddddd;
+    border-radius: 6px;
+  }
 `;
 const MessagesContentContainer = styled.div`
-    padding: 12px;
+  padding: 12px;
 `;
 const InputContainer = styled.div`
-    padding: 12px;
+  padding: 12px;
 `;
-setClassName('IndexContainer', IndexContainer.styledComponentId);
-setClassName('MessagesScrollContainer', MessagesScrollContainer.styledComponentId);
-setClassName('MessagesContentContainer', MessagesContentContainer.styledComponentId);
-setClassName('InputContainer', InputContainer.styledComponentId);
+setClassName("IndexContainer", IndexContainer.styledComponentId);
+setClassName(
+  "MessagesScrollContainer",
+  MessagesScrollContainer.styledComponentId
+);
+setClassName(
+  "MessagesContentContainer",
+  MessagesContentContainer.styledComponentId
+);
+setClassName("InputContainer", InputContainer.styledComponentId);
 
-const AIChatComponent: React.FC<ChatgptComponentProps> = ({ 
-    messages,
-    error,
-    input,
-    handleInputChange,
-    handleSubmit,
-    isLoading,
-    MeAvatar = DefaultMeAvatar,
-    GptAvatar = DefaultGptAvatar,
-    sendText
- }) => {
-    const MessageBoxComponent = getComponent('MessageBox') || MessageBox;
-    const ChatInputBoxComponent = getComponent('ChatInputBox') || ChatInputBox;
-    const BetweenMessageBoxAndChatInputBox = getSlot('BetweenMessageBoxAndChatInputBox')
-    console.log('BetweenMessageBoxAndChatInputBox', typeof BetweenMessageBoxAndChatInputBox)
+const AIChatComponent: React.FC<ChatgptComponentProps> = ({
+  messages,
+  error,
+  input,
+  handleInputChange,
+  handleSubmit,
+  isLoading,
+  MeAvatar = DefaultMeAvatar,
+  GptAvatar = DefaultGptAvatar,
+  sendText,
+}) => {
+  const MessageBoxComponent = getComponent("MessageBox") || MessageBox;
+  const ChatInputBoxComponent = getComponent("ChatInputBox") || ChatInputBox;
+  const BetweenMessageBoxAndChatInputBox = getSlot(
+    "BetweenMessageBoxAndChatInputBox"
+  );
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+  const [shouldScrollBottom, setShouldScrollBottom] = useState(true);
+  const scrollToBottomDirectly = () => {
+    const div = chatContainerRef.current;
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  };
+  useLayoutEffect(() => {
+    const div = chatContainerRef.current;
+    const checkScrollPosition = throttle(() => {
+      if (div) {
+        const { scrollTop, scrollHeight, clientHeight } = div;
+        setShouldScrollBottom(scrollTop + clientHeight >= scrollHeight - 50);
+      }
+    }, 200);
 
-    return (
-        <IndexContainer>
-            <MessagesScrollContainer>
-                <MessagesContentContainer>
-                    {messages.map(message => (
-                        <MessageBoxComponent 
-                            key={nanoid()} 
-                            side={message.role} 
-                            messageContent={message.content} 
-                            MeAvatar={MeAvatar}
-                            GptAvatar={GptAvatar}
-                            isLoading={isLoading}
-                            input={input}
-                            error={error}
-                            message={message}
-                        />
-                    ))}
-                </MessagesContentContainer>
-                {(isLoading && messages[messages.length - 1].role !== "assistant") && (
-                    <MessageBoxContainer side={"assistant"}>
-                    <Avatar avatar={GptAvatar}/>
-                    <Message side={"assistant"} messageContent={<Loading/>}/>
-                </MessageBoxContainer>
-                )}
-            </MessagesScrollContainer>
-            {BetweenMessageBoxAndChatInputBox ? <BetweenMessageBoxAndChatInputBox input={input} isLoading={isLoading} error={error}/> : null}
-            <InputContainer>
-                <ChatInputBoxComponent 
-                    sendText={sendText} 
-                    input={input} 
-                    handleInputChange={handleInputChange}
-                    handleSubmit={handleSubmit}
-                />
-            </InputContainer>
-        </IndexContainer>
-    )
+    div?.addEventListener("scroll", checkScrollPosition, { passive: true });
+    return () => div?.removeEventListener("scroll", checkScrollPosition);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (shouldScrollBottom) {
+      scrollToBottomDirectly();
+    }
+  }, [messages[messages.length - 1]?.content, shouldScrollBottom]);
+
+  return (
+    <IndexContainer>
+      <MessagesScrollContainer ref={chatContainerRef}>
+        <MessagesContentContainer>
+          {messages.map((message) => (
+            <MessageBoxComponent
+              key={message.id}
+              side={message.role}
+              messageContent={message.content}
+              MeAvatar={MeAvatar}
+              GptAvatar={GptAvatar}
+              isLoading={isLoading}
+              input={input}
+              error={error}
+              message={message}
+            />
+          ))}
+        </MessagesContentContainer>
+        {isLoading &&
+          messages &&
+          messages[messages.length - 1]?.role === "user" && (
+            <MessageBoxContainer side={"assistant"}>
+              <Avatar avatar={GptAvatar} />
+              <Message side={"assistant"} messageContent={<Loading />} />
+            </MessageBoxContainer>
+          )}
+      </MessagesScrollContainer>
+      {BetweenMessageBoxAndChatInputBox ? (
+        <BetweenMessageBoxAndChatInputBox
+          input={input}
+          isLoading={isLoading}
+          error={error}
+        />
+      ) : null}
+      <InputContainer>
+        <ChatInputBoxComponent
+          sendText={sendText}
+          input={input}
+          handleInputChange={handleInputChange}
+          handleSubmit={(...props) => {
+            scrollToBottomDirectly();
+            handleSubmit(...props);
+          }}
+        />
+      </InputContainer>
+    </IndexContainer>
+  );
 };
 
 export default AIChatComponent;
-export * as utils from './utils';
+export * as utils from "./utils";
--- lib/test.tsx
+++ lib/test.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import AIChatComponent, { utils } from "./index.tsx";
 import styled from "styled-components";
 import { useChat } from "ai/react";
+import { nanoid } from "nanoid";
 utils.setSlot("AfterMessage", ({ message }) => (
   <span>{message.createdAt?.toString()}</span>
 ));
@@ -14,12 +15,16 @@ const TestContainer = styled.div`
   height: calc(100vh - 20px);
   align-items: center;
   justify-content: center;
+  overflow-y: hidden;
 `;
 const TestFloatingDiv = styled.div`
   width: 500px;
   height: calc(100vh - 200px);
+  max-height: calc(100vh - 200px);
+  min-height: calc(100vh - 200px);
   background-color: white;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow-y: hidden;
   border-radius: 8px;
   .${utils.getClassName("SendButton")} {
     color: black;
@@ -31,15 +36,42 @@ const markdownString = `
 $$
 c = \\sqrt{a^2 + b^2}
 $$
+这是一个行内公式示例 $e^{i\pi} + 1 = 0$，以及一个块级公式示例：
+
+$$
+c = \\sqrt{a^2 + b^2}
+$$
+这是一个行内公式示例 $e^{i\pi} + 1 = 0$，以及一个块级公式示例：
+
+$$
+c = \\sqrt{a^2 + b^2}
+$$
 `;
 
 const Root = () => {
   const chat = useChat();
   useEffect(() => {
-    chat.setMessages([
-      { role: "user", content: "hello", id: "1" },
-      { role: "assistant", content: markdownString, id: "2" },
-    ]);
+    let messages: any[] = [];
+    setInterval(() => {
+      if (messages.length > 10) return;
+      if (messages.length % 2) {
+        messages = [
+          ...messages,
+          {
+            role: "assistant",
+            content: markdownString,
+            id: nanoid(),
+          },
+        ];
+        chat.setMessages(messages);
+      } else {
+        messages = [
+          ...messages,
+          { role: "user", content: "hello", id: nanoid() },
+        ];
+        chat.setMessages(messages);
+      }
+    }, 1000);
   }, []);
   return <AIChatComponent {...chat} />;
 };
